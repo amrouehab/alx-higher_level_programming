3ae1bad5fb1882afe50f56caab0c5ece2e959b31
amrouehab <amrou.ehab@gmail.com>
2024-09-09 19:50:20 -0700

    Add all changes including modified and new files

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,40 +1,28 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle
+ * check_cycle - checks if a singly linked list has a cycle in it
  * @list: pointer to the head of the list
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *hash_table[1024];
-	size_t i;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (list == NULL)
 		return (0);
 
-	/* Initialize the hash table */
-	for (i = 0; i < 1024; i++)
-		hash_table[i] = NULL;
-
-	/* Traverse the list */
-	while (list != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
-		/* Compute the hash index */
-		i = ((size_t)list) % 1024;
+		slow = slow->next;
+		fast = fast->next->next;
 
-		/* Check if the current node is already in the hash table */
-		if (hash_table[i] == list)
-			return (1); /* Cycle detected */
-
-		/* Store the current node in the hash table */
-		hash_table[i] = list;
-
-		/* Move to the next node */
-		list = list->next;
+		if (slow == fast)
+			return (1);
 	}
 
-	return (0); /* No cycle detected */
+	return (0);
 }
 
