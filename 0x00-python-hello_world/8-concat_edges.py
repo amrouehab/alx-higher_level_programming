36337cd54c65632fea88a05f6f75654b0bf67679
amrouehab <amrou.ehab@gmail.com>
2024-09-09 20:12:59 -0700

    Fix length issue in task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[103:111])
+ language that combines remarkable\
+ power with very clear syntax"
+print("".join([str[39:59], " ", str[60:72], " ", str[0:6], " ", str[71:77]]))
