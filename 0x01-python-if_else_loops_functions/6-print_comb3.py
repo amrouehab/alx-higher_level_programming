f82e717a9438c2d4ae51f9353754c8e912ef7b9f
amrouehab <amrou.ehab@gmail.com>
2024-09-10 21:45:18 -0700

    Fix task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        end_char = "\n" if i == 8 and j == 9 else ", "
-        print("{:02d}{:02d}".format(i, j), end=end_char)
+        if i == 8 and j == 9:
+            print("{:02d}{:02d}".format(i, j))
+        else:
+            print("{:02d}{:02d}".format(i, j), end=", ")
