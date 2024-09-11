90baf5db17f2f56ba2ba8ac7df17da592ab01c2b
amrouehab <amrou.ehab@gmail.com>
2024-09-10 21:29:45 -0700

    Fix Issues

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if chr(i) not in ('q', 'e'):
-        print(chr(i), end="")
+print("".join(chr(i) for i in range(97, 123) if chr(i) not in "qe"), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-	for char in str:
-		# Check if the character is lowercase (ASCII between 97 and 122)
-		if ord(char) >= ord('a') and ord(char) <= ord('z'):
-			# Convert lowercase to uppercase by subtracting 32
-			char = chr(ord(char) - 32)
-		# Print the character without a newline, followed by an empty space
-		print("{}".format(char), end="")
-	print()  # Print a newline at the end
+    for char in str:
+        # Check if the character is lowercase (ASCII between 97 and 122)
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            # Convert lowercase to uppercase by subtracting 32
+            char = chr(ord(char) - 32)
+        # Print the character without a newline, followed by an empty space
+        print("{}".format(char), end="")
+    print()  # Print a newline at the end
