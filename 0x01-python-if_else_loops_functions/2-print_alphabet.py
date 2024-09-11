a5e6e836d0741b1dee012634a4f8e81b31773343
amrouehab <amrou.ehab@gmail.com>
2024-09-10 21:19:17 -0700

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end="")
-print("")  # Print a newline at the end
+print("".join(chr(i) for i in range(ord('a'), ord('z') + 1)), end="")
