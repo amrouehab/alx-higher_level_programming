a4268a7446b998d792889bfa5f3c2e7cda82a317
amrouehab <amrou.ehab@gmail.com>
2024-09-18 19:21:16 -0700

    Fix Issues

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import hidden_4
+import hidden_4
 
-    for name in sorted(dir(hidden_4)):
-        if not name.startswith("__"):
-            print(name)
+if __name__ == "__main__":
+  pass  # Prevent execution when imported
+
+for name in sorted(dir(hidden_4)):
+    if not name.startswith("__"):
+        print(name)
