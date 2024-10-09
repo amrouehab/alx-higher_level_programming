9759256ba287350c11b422aebdd1525e7d311d6b
amrouehab <amrou.ehab@gmail.com>
2024-10-08 19:42:48 -0700

    Fix style error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width of the rectangle."""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -27,7 +27,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height of the rectangle."""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
