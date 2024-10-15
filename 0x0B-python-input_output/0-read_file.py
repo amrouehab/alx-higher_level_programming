2ceea56f9743d63f23b6b92617cf7d37c3fdef6c
amrouehab <amrou.ehab@gmail.com>
2024-10-15 16:57:07 -0700

    Tasks solving

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# Reads a file and prints its content
-
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r', encoding='utf-8') as f:
