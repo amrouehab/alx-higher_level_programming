8ea2026ecfe6aef99e606dc4bb53a12a79964d1b
amrouehab <amrou.ehab@gmail.com>
2024-10-15 17:48:56 -0700

    Fix docstring

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 '''
 This script reads stdin line by line and computes metrics.
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+Each 10 lines and after a keyboard interruption (CTRL + C), prints those statistics since the beginning:
+Total file size: File size: <total size>
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+if a status code doesn’t appear, don’t print anything for this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
 '''
 
 import sys
